1f4d89327fae4cfb6bfa6282adbbeb8a860455a3
SanushaG <sgalappathy@gmail.com>
2019-08-02 01:08:16 -0400

    javascript user input validation

--- www/scripts/login.js
+++ www/scripts/login.js
@@ -111,36 +111,61 @@ function login(){
 //     const firstName = document.getElementById("firstName").value;
 //     const lastName = document.getElementById("lastName").value;
 
-//     if (tryPassword == confirmedPassword) {
-//         const password = tryPassword;
-//         const dateCreated = Date.today().toFormat("YYYY-MM-DD");
-//         firebase.auth().createUserWithEmailAndPassword(email, password).then(
-//             () => {
-//                 //Save information for the user
-//                 const sUser = firebase.auth().currentUser.uid;
-//                 firebase.database().ref("user/" + sUser).set({
-//                     userType: "pending",
-//                     email: email,
-//                     password: password,
-//                     firstName: firstName,
-//                     lastName: lastName,
-//                     dateCreated: dateCreated
-//                 });
-//                 //Then redirect to payment page
-//                 // window.location.replace("../pages/payment.html");
+//     if (validateEmail(email || email == "")) {
+//         if (validateTextOnly(firstName) || firstName == "") {
+//             if (validateTextOnly(lastName)) {
+//                 if (tryPassword == confirmedPassword || tryPassword == "" || confirmedPassword == "") {
+//                     const password = tryPassword;
+//                     const dateCreated = Date.today().toFormat("YYYY-MM-DD");
+//                     firebase.auth().createUserWithEmailAndPassword(email, password).then(
+//                         () => {
+//                             //Save information for the user
+//                             const sUser = firebase.auth().currentUser.uid;
+//                             firebase.database().ref("user/" + sUser).set({
+//                                 userType: "pending",
+//                                 email: email,
+//                                 password: password,
+//                                 firstName: firstName,
+//                                 lastName: lastName,
+//                                 dateCreated: dateCreated
+//                             });
+//                             //Then redirect to payment page
+//                             document.getElementById("signUpError").innerHTML = "It worked";
+//                             // window.location.replace("../pages/payment.html");
+//                         }
+//                     ).catch(function (error) {
+//                         // Handle Errors here.
+//                         var errorCode = error.code;
+//                         var errorMessage = error.message;
+//                         document.getElementById("signUpError").innerHTML = "Sign Up Error: " + errorCode + ". " + errorMessage;
+//                         console.log("Something went wrong:" + errorCode + "." + errorMessage);
+//                     });
+//                 }
+//                 else {
+//                     document.getElementById("signUpError").innerHTML = "The passwords don't seem to match! Please try again";
+//                 }
 //             }
-//         ).catch(function (error) {
-//             // Handle Errors here.
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
-//             document.getElementById("signUpError").innerHTML = "Sign Up Error: " + errorCode + ". " + errorMessage;
-//             console.log("Something went wrong:" + errorCode + "." + errorMessage);
-//         });
+//             else {
+//                 document.getElementById("signUpError").innerHTML = "Last name must be filled and can only be letters";
+//             }
+//         } else {
+//             document.getElementById("signUpError").innerHTML = "First name must be filled can only be letters";
+//         }
+//     } else {
+//         document.getElementById("signUpError").innerHTML = "Looks like either that wasn't an email address or that field was left empty... Let's try again!";
 //     }
-//     else {
-//         document.getElementById("signUpError").innerHTML = "The passwords don't seem to match! Please try again";
-//     }
-//});
+// });
+
+// //Regex for Email - Validate email is well formed
+// function validateEmail(email) {
+//     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(email);
+// }
+
+// function validateTextOnly(name) {
+//     var re = /^[a-zA-Z]+$/;
+//     return re.test(name);
+// }
 ///////////////////
 ///////////// Verify User
 
